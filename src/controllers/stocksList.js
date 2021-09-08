0f8af4864184adf517249edd01622149b5fac02e
SamRC <samrcspace@gmail.com>
2021-09-08 14:49:42 +0530

    Add nifty50 symbol list

--- src/controllers/stocksList.js
+++ src/controllers/stocksList.js
@@ -1,9 +1,14 @@
 const stocksListController = require("express").Router();
 
 const nseStocksList = require("../../data/nse-stocks-list");
+const nifty50StocksList = require("../../data/nifty50-stocks-list");
 
 stocksListController.get("/", async (req, res) => {
   return res.json(nseStocksList);
+});
+
+stocksListController.get("/nifty50", async (req, res) => {
+  return res.json(nifty50StocksList);
 });
 
 module.exports = stocksListController;
